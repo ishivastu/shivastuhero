91caf9d8c3a6e52f35fbd31fef9f641713d3c6cf
shivastu Mishra <shivastumishra@gmail.com>
2026-02-01 01:45:21 +0530

    backend

---
+++ index.js
@@ -0,0 +1,30 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+// ✅ middlewares
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// ✅ test route
+app.get("/api/health", (req, res) => {
+  res.json({
+    name: "shivastu",
+    age: 22
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`App is listening on port ${PORT}`);
+});
