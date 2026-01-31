14cfaff0b7beffc343f6a339b0e726d8750263e5
shivastu Mishra <shivastumishra@gmail.com>
2026-02-01 01:56:03 +0530

    changed link

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 // ✅ middlewares
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://ishivastu.github.io/mylove/",
   credentials: true
 }));
 
