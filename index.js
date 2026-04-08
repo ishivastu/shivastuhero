9e41033071e9a6c4ee34df679f842e306802c87f
Shivastu Mishra <shivastumishra@gmail.com>
2026-04-08 12:10:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ import cors from "cors";
 const app = express();
 
 const PORT = process.env.PORT || 5000;
-
-// ✅ middlewares
 app.use(cors({
   origin: [
     "http://localhost:5173",
@@ -22,7 +20,6 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// ✅ test route
 app.get("/api/health", (req, res) => {
   res.json({
     name: "shivastu",
